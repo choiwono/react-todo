6d5414bd710683b92a781dc4554837347a578043
choiwono <treasure_b@naver.com>
2020-07-12 20:16:58 +0900

    :sparkles: todo 기본 코딩

---
+++ todo/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Header } from './components/layout/Header' 
+
+export const App = () => {
+  
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
